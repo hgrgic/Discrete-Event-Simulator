b557a0c2d3c3e5df8407e4c6aa6c241c3e1773bf
Hrvoje Grgic <hrvoje.grgic1996@gmail.com>
2020-01-10 12:48:00 +0300

    Operational build

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from main import app
+
 from flask_restful import Api
+from flask_restful_swagger import swagger
 
-from main.core.web.controller.ConfigurationWebController import ConfigurationWebController
-from main.core.web.controller.SimulationWebController import SimulationController
+from main.core.web.controller.ReportsWebController import ReportsWebController
+from main.core.web.controller.ReportWebController import ReportWebController
+from main.core.web.controller.SimulationWebController import SimulationWebController
+from main.core.web.controller.SimulationsWebController import SimulationsWebController
 
-app = Flask(__name__)
-api = Api(app)
 
-api.add_resource(ConfigurationWebController, '/config')
-api.add_resource(SimulationController, '/simulation')
+api = swagger.docs(Api(app), apiVersion='0.1',
+                   description='ING Discrete Event Simulator',
+                   api_spec_url='/docs',
+                   basePath='http://localhost:8080')
 
-app.run(port='8080')
-
-print ("APIs:", api.urls.values())
+api.add_resource(SimulationWebController, '/simulation')
+api.add_resource(SimulationsWebController, '/simulations')
+api.add_resource(ReportWebController, '/report')
+api.add_resource(ReportsWebController, '/reports')
